06279933e14b07b9c54c83c638098ee8f8ef352d
rachoac <aron.racho@gmail.com>
2015-10-29 21:33:49 -0700

    cleaning up dropdowns

--- client/src/components/state-field.jsx
+++ client/src/components/state-field.jsx
@@ -69,18 +69,23 @@ export default React.createClass({
         ];
     },
 
+    getFieldName() {
+        return this.props.fieldName.split(".")[1];
+    },
+
     render: function() {
         return(
             <DropdownField
                 {...this.props}
                 required={this.props.required}
                 label={this.props.label}
-                selectValue={this.props.data[this.props.fieldName]}
-                field={this.props.fieldName}
+                selectValue={this.props.data[this.getFieldName()]}
+                fieldName={this.props.fieldName}
+                ref={this.props.ref}
                 onChange={this.props.onChange}>
-                    {this.getStates().map(entry =>
-                        <option key={entry.abbreviation} value={entry.abbreviation}>{entry.abbreviation}</option>
-                    )}
+                {this.getStates().map(entry =>
+                    <option key={entry.abbreviation} value={entry.abbreviation}>{entry.abbreviation}</option>
+                )}
             </DropdownField>
         );
     }
--- client/src/sections/contact-fields.jsx
+++ client/src/sections/contact-fields.jsx
@@ -12,107 +12,89 @@ export default class ContactFields extends BaseSection {
     render() {
         return (
             <div>
-                <div>
-                    <ValidatedInput
-                        {...this.props}
-                        required={true}
-                        label="Address 1" type="text"
-                        ref="data.q_address_1"
-                        fieldName="data.q_address_1"
-                        value={this.props.data.q_address_1}
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <ValidatedInput
+                    {...this.props}
+                    required={true}
+                    label="Address 1" type="text"
+                    ref="data.q_address_1"
+                    fieldName="data.q_address_1"
+                    value={this.props.data.q_address_1}
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <ValidatedInput
-                        {...this.props}
-                        label="Address 2"
-                        type="text"
-                        ref="data.q_address_2"
-                        fieldName="data.q_address_2"
-                        value={this.props.data.q_address_2}
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <ValidatedInput
+                    {...this.props}
+                    label="Address 2"
+                    type="text"
+                    ref="data.q_address_2"
+                    fieldName="data.q_address_2"
+                    value={this.props.data.q_address_2}
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <ValidatedInput
-                        {...this.props}
-                        required={true}
-                        label="City" type="text"
-                        ref="data.q_address_city"
-                        fieldName="data.q_address_city"
-                        value={this.props.data.q_address_city}
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <ValidatedInput
+                    {...this.props}
+                    required={true}
+                    label="City" type="text"
+                    ref="data.q_address_city"
+                    fieldName="data.q_address_city"
+                    value={this.props.data.q_address_city}
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <StateField
-                        {...this.props}
-                        required={true}
-                        label="State/Province"
-                        ref="data.q_address_state"
-                        fieldName="data.q_address_state"
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <StateField
+                    {...this.props}
+                    required={true}
+                    label="State/Province"
+                    ref="data.q_address_state"
+                    fieldName="data.q_address_state"
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <ValidatedInput
-                        {...this.props}
-                        required={true}
-                        label="Zip" type="text"
-                        ref="data.q_address_zip"
-                        fieldName="data.q_address_zip"
-                        value={this.props.data.q_address_zip}
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <ValidatedInput
+                    {...this.props}
+                    required={true}
+                    label="Zip" type="text"
+                    ref="data.q_address_zip"
+                    fieldName="data.q_address_zip"
+                    value={this.props.data.q_address_zip}
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <CountryField
-                        {...this.props}
-                        required={true}
-                        label="Country"
-                        ref="data.q_address_country"
-                        fieldName="data.q_address_country"
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <CountryField
+                    {...this.props}
+                    required={true}
+                    label="Country"
+                    ref="data.q_address_country"
+                    fieldName="data.q_address_country"
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <Input
-                        label="Mobile phone"
-                        type="text"
-                        value={this.props.data.q_phone_mobile}
-                        ref="data.q_phone_mobile"
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <Input
+                    label="Mobile phone"
+                    type="text"
+                    value={this.props.data.q_phone_mobile}
+                    ref="data.q_phone_mobile"
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <Input
-                        label="Home phone"
-                        type="text"
-                        value={this.props.data.q_phone_home}
-                        ref="data.q_phone_home"
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <Input
+                    label="Home phone"
+                    type="text"
+                    value={this.props.data.q_phone_home}
+                    ref="data.q_phone_home"
+                    onChange={this.handleChange}
+                />
 
-                <div>
-                    <ValidatedInput
-                        {...this.props}
-                        label="Email address" type="text"
-                        value={this.props.data.q_email}
-                        required={true}
-                        ref="data.q_email"
-                        fieldName="data.q_email"
-                        onChange={this.handleChange}
-                    />
-                </div>
+                <ValidatedInput
+                    {...this.props}
+                    label="Email address" type="text"
+                    value={this.props.data.q_email}
+                    required={true}
+                    ref="data.q_email"
+                    fieldName="data.q_email"
+                    onChange={this.handleChange}
+                />
             </div>
         );
     }
