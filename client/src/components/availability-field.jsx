c9cdbda9ec6ba1fe4583c4994bfd066e89733ee2
rachoac <aron.racho@gmail.com>
2015-10-29 23:15:39 -0700

    availability field control

--- client/src/components/availability-field.jsx
+++ client/src/components/availability-field.jsx
@@ -3,20 +3,79 @@ import React from 'react';
 import Input from 'react-bootstrap/lib/Input';
 import DropdownField from './dropdown-field.jsx';
 
-export default React.createClass({
-    render: function() {
+class DayLength extends React.Component {
+
+    render() {
+        return (
+            <select
+                value={this.props.dayLength}
+                onChange={this.props.onChange}>
+                <option value="full">Full day</option>
+                <option value="half">Half day</option>
+            </select>
+        )
+    }
+}
+
+class DayHalf extends React.Component {
+    render() {
+        return (
+            <select>
+                <option>AM</option>
+                <option>PM</option>
+            </select>
+        )
+    }
+}
+
+export default class AvailabilityField extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dayLength : "full",
+            canDo : "false"
+        }
+    }
+
+    changeDayLength(field) {
+        this.setState( {
+            dayLength: field.target.value
+        });
+    }
+
+    changeCanDo(field) {
+        this.setState( {
+            canDo: "" + field.target.checked
+        });
+    }
+
+    render() {
+        var dayHalf;
+        if ( this.state.dayLength === 'half') {
+            dayHalf = <DayHalf />;
+        }
+
+        var dayLength;
+        if ( this.state.canDo === "true" ) {
+            dayLength = <DayLength {...this.props}
+                onChange={this.changeDayLength.bind(this)}
+                value={this.state.dayLength}
+            />;
+        }
+
         return (
             <div className="availability-field">
-                <Input label={this.props.label} type="checkbox"/>
-                <select>
-                    <option>Full day</option>
-                    <option>Half day</option>
-                </select>
-                <select>
-                    <option>AM</option>
-                    <option>PM</option>
-                </select>
+
+                <Input
+                    label={this.props.label}
+                    type="checkbox"
+                    value="true"
+                    onChange={this.changeCanDo.bind(this)}
+                />
+                {dayLength}
+                {dayHalf}
             </div>
         );
     }
-});
+}
