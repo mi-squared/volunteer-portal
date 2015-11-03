a763ecd8e984ceabd95363023318e1567bcc8af6
rachoac <aron.racho@gmail.com>
2015-11-03 02:25:45 -0800

    Adding specialties

--- client/src/sections/medical-fields.jsx
+++ client/src/sections/medical-fields.jsx
@@ -3,6 +3,7 @@ import ReactAddons from 'react/addons';
 import Input from 'react-bootstrap/lib/Input';
 
 import ValidatedInput from '../components/validated-input-field.jsx';
+import MultiselectField from '../components/multiselect-field.jsx';
 import BaseSection from './base-section.jsx'
 
 export default class MedicalFields extends BaseSection {
@@ -12,13 +13,40 @@ export default class MedicalFields extends BaseSection {
 
                 <hr/>
 
-                <ValidatedInput
-                    {...this.props}
-                    label="List any specialties:"
-                    value={this.props.data.q_medical_specialties}
-                    fieldName="data.q_medical_specialties"
-                    ref="data.q_medical_specialties"
-                    onChange={this.handleChange}/>
+                <div>
+                    <MultiselectField
+                        {...this.props}
+                        label="List any specialties:"
+                        value={this.props.data.q_medical_specialties}
+                        placeHolder="Click to choose specialties"
+                        ref="data.q_medical_specialties"
+                        fieldName="data.q_medical_specialties"
+                        onChange={this.handleChange}>
+
+                        <option>Primary care</option>
+                        <option>Internal Medicine</option>
+                        <option>Pediatrics</option>
+                        <option>Pulmonary</option>
+                        <option>Cardiiology</option>
+                        <option>Endocrinology</option>
+                        <option>Infectious Disease</option>
+                        <option>GI</option>
+                        <option>Obstetrics & Gynecology</option>
+                        <option>Gyn Onc</option>
+                        <option>Urogynecology</option>
+                        <option>General Surgery</option>
+                        <option>Urology</option>
+                        <option>Podiatry</option>
+                        <option>Colorectal Surgery</option>
+                        <option>Dermatology</option>
+                        <option>Plastic Surgery</option>
+                        <option>Opthalmology</option>
+                        <option>Radiology</option>
+                        <option>Pharmacy</option>
+                        <option>Medical Laboratory</option>
+
+                    </MultiselectField>
+                </div>
 
                 <ValidatedInput
                     {...this.props}
