563960dcb072f4f76272548180510b91c7beae58
rachoac <aron.racho@gmail.com>
2015-11-04 22:18:25 -0800

    Fixing dob fields

--- client/src/pages/main-page.jsx
+++ client/src/pages/main-page.jsx
@@ -44,7 +44,7 @@ class MainPage extends React.Component {
                 },
                 q_dob: {
                     type: 'string',
-                    format: "date",
+                    pattern: /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/,
                     required: true
                 },
                 q_address_1: {
