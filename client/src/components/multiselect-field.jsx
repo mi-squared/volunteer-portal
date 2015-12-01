817b451f882d99cda98be750e2b408c94144165d
ryan broughan <ryan.broughan@gmail.com>
2015-11-30 16:24:40 -0800

    refactored getValue() into handle change

--- client/src/components/multiselect-field.jsx
+++ client/src/components/multiselect-field.jsx
@@ -1,65 +1,84 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-
 import classNames from 'classnames'
 
-export default React.createClass({
+// export default React.createClass({
 
-    getValue: function() {
-        return this.props.value ? this.props.value.split(","): []
-    },
+class MultiselectField extends React.Component {
 
-    commitChange: function(e) {
-        if ( this.props.onChange ) {
-            this.props.onChange(this.props.fieldName, $(e.target).val().join(","));
-        }
-    },
+  constructor(props) {
+   super(props);
+   this.handleChange = this.handleChange.bind(this);
+   this.commitChange = this.commitChange.bind(this);
+   this.state = {
+     value: []
+   };
+ }
 
-    componentDidMount: function() {
-        var targetField = $(ReactDOM.findDOMNode(this)).find("select");
-        targetField.select2(
-            { placeholder : this.props.placeHolder }
-        );
+  getValue() {
+      return this.props.value ? this.props.value.split(","): []
+  }
 
-        var self = this;
-        targetField.on("select2:select", function (e) {
-            self.commitChange(e);
-        });
-    },
+  handleChange(event) {
+    this.setState({value: event.target.value.split(",")})
+  }
 
-    render: function() {
-        var requiredLabel = this.props.required ? <span className='j-required'>*</span> : '';
-        var message;
-        var fieldClassName = "form-control";
-        if (this.props.errorFields) {
-            var errorField = this.props.errorFields[this.props.fieldName];
-            if ( errorField ) {
-                message = errorField['message'];
-                fieldClassName += " error";
-            }
-        }
-        var errorMessage = message ? <span className='j-error-message'>{message}</span> : '';
+  commitChange(e) {
+      if ( this.props.onChange ) {
+          this.props.onChange(this.props.fieldName, $(e.target).val().join(","));
+      }
+  }
 
-        return (
-            <div className={classNames('form-group', message ? 'has-error' : '')}>
-                <label className="control-label">
-                    <span>{requiredLabel}{this.props.label}</span>
-                </label>
-                {errorMessage}
-                <div>
-                    <select
-                        multiple="true"
-                        className={fieldClassName}
-                        ref={this.props.ref}
-                        value={this.getValue()}
-                        fieldName={this.props.fieldName}
-                        style={{width: "100%"}}>
-                        {this.props.children.map(entry =>
-                            <option key={entry.props.value || entry.props.children} value={entry.props.value || entry.props.children}>{entry.props.children}</option>
-                        )}
-                    </select>
-                </div>
-            </div>
-        );
-    }
-});
+  componentDidMount() {
+      var targetField = $(ReactDOM.findDOMNode(this)).find("select");
+      targetField.select2(
+          { placeholder : this.props.placeHolder }
+      );
+
+      var self = this;
+      targetField.on("select2:select", function (e) {
+          self.commitChange(e);
+      });
+  }
+
+  render() {
+      var requiredLabel = this.props.required ? <span className='j-required'>*</span> : '';
+      var message;
+      var value = this.state.value;
+      var fieldClassName = "form-control";
+      if (this.props.errorFields) {
+          var errorField = this.props.errorFields[this.props.fieldName];
+          if ( errorField ) {
+              message = errorField['message'];
+              fieldClassName += " error";
+          }
+      }
+      var errorMessage = message ? <span className='j-error-message'>{message}</span> : '';
+
+      return (
+          <div className={classNames('form-group', message ? 'has-error' : '')}>
+              <label className="control-label">
+                  <span>{requiredLabel}{this.props.label}</span>
+              </label>
+              {errorMessage}
+              <div>
+                  <select
+                      multiple="true"
+                      className={fieldClassName}
+                      ref={this.props.ref}
+                      value={value}
+                      onChange={this.handleChange}
+                      fieldName={this.props.fieldName}
+                      style={{width: "100%"}}>
+                      {this.props.children.map(entry =>
+                          <option key={entry.props.value || entry.props.children} value={entry.props.value || entry.props.children}>{entry.props.children}</option>
+                      )}
+                  </select>
+              </div>
+          </div>
+      );
+  }
+}
+// );
+
+export default MultiselectField;
