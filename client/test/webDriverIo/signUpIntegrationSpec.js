30bd6fa9d96a25de1514f0b50c388ff9b4f8242b
ryan broughan <ryan.broughan@gmail.com>
2015-12-01 10:50:23 -0800

    Add basic selenium test for input# on NewVolunteerPage

---
+++ client/test/webDriverIo/signUpIntegrationSpec.js
@@ -0,0 +1,27 @@
+var webdriverio = require('webdriverio');
+var options = { desiredCapabilities: { browserName: 'chrome' } };
+var client = webdriverio.remote(options);
+var chai = require('chai');
+var assert = chai.assert;
+
+var pageUrl = "http://localhost:8080/#/new-volunteer";
+
+describe('NewVolunteerPage', () => {
+
+  afterEach(function(done) {
+    client.end(done);
+  });
+
+  it("displays form with 12 input fields and 2 selects", (done) => {
+    client
+        .init()
+        .url(pageUrl)
+        .elements('input', (err, elements) => {
+          assert.strictEqual(elements.value.length, 12);
+        })
+        .elements('select', (err, elements) => {
+          assert.strictEqual(elements.value.length, 2);
+        })
+        .call(done);
+  });
+});
