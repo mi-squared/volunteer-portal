55fa28da17cc47e195e1f118895c4c64c078184e
ryan broughan <ryan.broughan@gmail.com>
2015-12-07 13:59:22 -0800

    Create basic controller framework

---
+++ service/app/Http/Controllers/ListsController.php
@@ -0,0 +1,15 @@
+<?php namespace App\Http\Controllers;
+
+use App\List;
+use App\Option;
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+
+class ListsController extends BaseController
+{
+  
+
+
+}
+
+?>
