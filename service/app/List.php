1f1d2d71bdf0b472f102ccd0ebc451600aaa4505
ryan broughan <ryan.broughan@gmail.com>
2015-12-07 13:52:39 -0800

    Add list_id to  array on Option

--- service/app/List.php
+++ service/app/List.php
@@ -1,5 +1,7 @@
 <?php namespace App;
 
+use Illuminate\Database\Eloquent\Model;
+
   class List extends Model
   {
     protected $table = "lists";
--- service/app/Option.php
+++ service/app/Option.php
@@ -1,10 +1,13 @@
 <?php namespace App;
 
+use Illuminate\Database\Eloquent\Model;
+
   class Option extends Model
   {
     protected $table = "options";
 
     protected $fillable = [
+      'list_id',
       'name',
       'value',
       'order'
