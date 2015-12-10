1a97228ad0514349b2e538bb09eac325c21021f2
rachoac <aron.racho@gmail.com>
2015-12-09 22:56:05 -0800

    Adding logout & header

---
+++ client/src/components/header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.logout = this.logout.bind(this);
+    }
+
+    logout() {
+        this.props.reset();
+        this.props.history.pushState(null, '/');
+    }
+
+    render() {
+        return (
+            <div className="container well">
+                <div className="col-md-12" style={{"text-align" : "right"}}>
+                    <div>{this.props.session.f_username} <button onClick={this.logout}>logout</button></div>
+                </div>
+            </div>
+        );
+    }
+}
+
